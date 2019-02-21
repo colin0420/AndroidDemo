cd111c65553e59977bff55c752674d48e7769601
colin0420 <429127928@qq.com>
2019-02-22 00:01:51 +0800

    【Story完成】新闻列表app

--- app/src/main/java/com/example/androiddemo/MainActivity.java
+++ app/src/main/java/com/example/androiddemo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.androiddemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override public void onClick(View v) {
+                //NewsContentActivity.actionStart();
+            }
+        });
     }
 }
